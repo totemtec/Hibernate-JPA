9bd34b8530c2351a804622ab4995c3610542be89
majianglin <majianglin@totemtec.com>
2020-06-27 21:50:23 +0800

    Optional

---
+++ Optional/README.md
@@ -0,0 +1,9 @@
+**[在实体和查询中使用 Java 8 `Optional` ](https://github.com/totemtec/Hibernate-JPA/tree/master/Optional)**
+ 
+**说明:**  
+
+**要点:**
+- 使用Spring Data内置的查询方法，接收 `Optional` 返回结果 (例如, `findById()`)
+- 实现自己的查询方法，返回 `Optional`
+- 在实体类getter方法中使用 `Optional`
+- 模拟不同的场景，可以查看数据文件, `data-mysql.sql`
--- Optional/src/main/java/com/bookstore/MainApplication.java
+++ Optional/src/main/java/com/bookstore/MainApplication.java
@@ -1,9 +1,6 @@
 package com.bookstore;
 
-import com.bookstore.entity.Author;
-import com.bookstore.entity.Book;
 import com.bookstore.service.BookstoreService;
-import java.util.List;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,20 +17,14 @@ public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
-        System.exit(0);
     }
 
     @Bean
     public ApplicationRunner init() {
         return args -> {
 
-            List<Author> authors = bookstoreService.fetchAuthorWithBooks();
-            authors.forEach(a -> System.out.println("Author name: "
-                    + a.getName() + " Books: " + a.getBooks()));
-
-            List<Book> books = bookstoreService.fetchBookWithAuthor();
-            books.forEach(b -> System.out.println("Book title: "
-                    + b.getTitle() + " Author: " + b.getAuthor()));
+            bookstoreService.fetchAuthorById();
+            bookstoreService.fetchAuthorFromBook();
         };
     }
 }
