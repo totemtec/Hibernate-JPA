d968325dab6dc12f223a5531422421665f4509aa
majianglin <majianglin@totemtec.com>
2020-06-28 20:14:14 +0800

    LeftJoinFetch

--- LeftJoinFetch/src/main/java/com/bookstore/entity/Author.java
+++ LeftJoinFetch/src/main/java/com/bookstore/entity/Author.java
@@ -3,10 +3,7 @@ package com.bookstore.entity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 @Entity
 public class Author implements Serializable {
