f9aab5e3aec9388d16982a203abbb23f1c68d84f
majianglin <majianglin@totemtec.com>
2020-06-30 23:27:09 +0800

    JoinFetch

--- JoinFetch/src/main/resources/data-mysql.sql
+++ JoinFetch/src/main/resources/data-mysql.sql
@@ -1,10 +1,34 @@
-insert into author (age, name, genre, id) values (23, "Mark Janel", "Anthology", 1);
-insert into author (age, name, genre, id) values (43, "Olivia Goy", "Horror", 2);
-insert into author (age, name, genre, id) values (51, "Quartis Young", "Anthology", 3);
-insert into author (age, name, genre, id) values (34, "Joana Nimar", "History", 4);
-insert into author (age, name, genre, id) values (38, "Alicia Tom", "Anthology", 5);
-insert into author (age, name, genre, id) values (56, "Katy Loin", "Anthology", 6);
-insert into book (isbn, title, author_id, id) values ("001-JN", "A History of Ancient Prague", 4, 1);
-insert into book (isbn, title, author_id, id) values ("002-JN", "A People's History", 4, 2);
-insert into book (isbn, title, author_id, id) values ("001-MJ", "The Beatles Anthology", 1, 3);
-insert into book (isbn, title, author_id, id) values ("001-OG", "Carrie", 2, 4);
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE `book` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(60) NOT NULL,
+  `isbn` varchar(60) NOT NULL,
+  `author_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
+
+BEGIN;
+INSERT INTO `book` VALUES (1, 'A History of Ancient Prague', '001-JN', 4);
+INSERT INTO `book` VALUES (2, 'A Peoples History', '002-JN', 4);
+INSERT INTO `book` VALUES (3, 'The Beatles Anthology', '001-MJ', 1);
+INSERT INTO `book` VALUES (4, 'Carrie', '001-OG', 2);
+COMMIT;
+
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE `author` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `age` int(11) NOT NULL,
+  `genre` varchar(60) DEFAULT NULL,
+  `name` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
+BEGIN;
+INSERT INTO `author` VALUES (1, 23, 'Anthology', 'Mark Janel');
+INSERT INTO `author` VALUES (2, 43, 'Horror', 'Olivia Goy');
+INSERT INTO `author` VALUES (3, 51, 'Anthology', 'Quartis Young');
+INSERT INTO `author` VALUES (4, 34, 'History', 'Joana Nimar');
+INSERT INTO `author` VALUES (5, 38, 'Anthology', 'Alicia Tom');
+INSERT INTO `author` VALUES (6, 56, 'Anthology', 'Katy Loin');
+COMMIT;
