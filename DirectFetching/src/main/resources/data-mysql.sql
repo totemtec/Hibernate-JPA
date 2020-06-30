bba72f1c5bdf0d8109cf179d89b5ccf8bc65fddd
majianglin <majianglin@totemtec.com>
2020-06-30 23:27:03 +0800

    DirectRetching

--- DirectFetching/src/main/resources/data-mysql.sql
+++ DirectFetching/src/main/resources/data-mysql.sql
@@ -1,1 +1,13 @@
-insert into author (age, name, genre, id) values (23, "Mark Janel", "Anthology", 1);
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE `author` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `age` int(11) NOT NULL,
+  `genre` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
+BEGIN;
+INSERT INTO `author` VALUES (1, 23, 'Anthology', 'Mark Janel');
+INSERT INTO `author` VALUES (2, 43, 'Horror', 'Olivia Goy');
+COMMIT;
